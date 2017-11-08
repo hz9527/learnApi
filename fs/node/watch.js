cfa29c74724849d03930f5897b2874ce1acdca42
hz9527 <981379316@qq.com>
2017-11-09 00:09:46 +0800

    :sparkles: sprite fs

--- fs/node/watch.js
+++ fs/node/watch.js
@@ -6,14 +6,51 @@ var images = require('images')
 
 var resolve = (p = '') => path.join(__dirname, './watch' + p)
 
-function sprite () {
-  fs.readdir(resolve('/assets'), (err, files) => {
-    if (!fs.existsSync(resolve('/dist'))) {
-      fs.mkdirSync(resolve('/dist'))
+function sprite (input, output, padding) {
+  fs.readdir(resolve('/' + input), (err, files) => {
+    if (!fs.existsSync(resolve('/' + output))) {
+      fs.mkdirSync(resolve('/' + output))
     }
-
+    let list = []
+    let tw = 0
+    let th = 0
+    let cur = padding
+    let spr = null
+    let i = 0
+    let css = ''
+    files.forEach(name => {
+      let img = images(resolve('/' + input + '/' + name))
+      let w = img.width()
+      let h = img.height()
+      list.push({
+        width: w,
+        height: h
+      })
+      tw = tw > w ? tw : w
+      th += h + padding * 2
+      images.gc()
+    })
+    spr = images(tw, th)
+    while (i < list.length) {
+      spr.draw(images(resolve('/' + input + '/' + files[i])), 0, cur)
+      css += `.icon-${files[i].replace('.png', '')} {
+        width: ${list[i].width}px;
+        height: ${list[i].height}px;
+        background-image: url('./sprite.png');
+        background-position: 0 -${cur}px;
+      }
+      `
+      cur += list[i].height + padding
+      i++
+    }
+    spr.save(resolve('/' + output + '/sprite.png'))
+    fs.writeFile(resolve('/' + output + '/sprite.css'), css, err => {
+      if (err) {
+        console.log(err)
+      }
+    })
   })
 }
 
 
-sprite()
+sprite('assets', 'dist', 5)
